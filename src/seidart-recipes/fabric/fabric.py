f5984fd071ffc7fbc7e1853c1051434d604ca46e
sbernsen <stevenbernsen@gmail.com>
2024-05-31 16:08:55 -0400

    fabric.py recipe update

--- src/seidart-recipes/fabric/fabric.py
+++ src/seidart-recipes/fabric/fabric.py
@@ -3,13 +3,27 @@ import pandas as pd
 from glob2 import glob  
 from seidart.routines.fabricsynth import Fabric 
 
+# This recipe replicates a couple fabrics from Figure 7 from the following paper:
 # https://tc.copernicus.org/articles/15/303/2021/
+#
+# The parameters were tuned somewhat heuristically to match the figure
 
-plot = True
-sp_strongnorm = {
+multipole1 = {
     'distribution': 'normal',
     'multimodal': True,
-    'npts': [155, 320, 80, 150, 110, 110, 110],
+    'npts': [160, 175, 160, 90, 90],
+    'strike': [110, 290, 255, 75, 50],
+    'strike_std': [10, 10, 8, 5, 6],
+    'dip': [80, 90, 80, 65, 72],
+    'dip_std': [5, 20, 25, 5, 5],
+    'skew_strike': [0, 15, -8, 0, 0],
+    'skew_dip': [0, -20, -8, 0, 0]
+}
+
+multipole2 = {
+    'distribution': 'normal',
+    'multimodal': True,
+    'npts': [155, 320, 85, 150, 110, 110, 110],
     'strike': [275, 80, 295, 115, 115, 195, 230],
     'strike_std': [5, 7, 10, 5, 35, 10, 10],
     'dip': [75, 75, 65, 75, 50, 65, 75],
@@ -18,43 +32,23 @@ sp_strongnorm = {
     'skew_dip': [0, 0, 0, 0, -15, 0, 0]
 }
 
-sps = Fabric(
-    sp_strongnorm, plot = False
+# We can plot now or we can plot later. 
+mp1 = Fabric(
+    multipole1, plot = True
 )
-sps.cmap = 'cool'
-sps.alpha = 0.2 
-sps.marker_size = 1 
-sps.projection_plot()
 
-# sps_unif = {
-#     'distribution': 'uniform',
-#     'npts': 100,
-#     'strike': 90,
-#     'strike_std': 15,
-#     'dip': 30,
-#     'dip_std': 10,
-#     'skew_strike': 10,
-#     'skew_dip': 5
-# }
+mp2 = Fabric(
+    multipole2, plot = True
+)
 
-# mps_unif = {
-#     'distribution': 'uniform',
-#     'npts': 100,
-#     'strike': 90,
-#     'strike_std': 15,
-#     'dip': 30,
-#     'dip_std': 10,
-#     'skew_strike': 10,
-#     'skew_dip': 5
-# }
+mp3 = Fabric(
+    multipole2, plot = True
+)
+# mp1.cmap_name = 'gist_heat_r'
+# mp1.custom_cmap()
+# mp1.alpha = 0.3 
+# mp1.marker_size = 1 
+# mp1.projection_plot()
 
-# mps_norm = {
-#     'distribution': 'normal',
-#     'npts': [100,220,400],
-#     'strike': 90,
-#     'strike_std': 15,
-#     'dip': 30,
-#     'dip_std': 10,
-#     'skew_strike': 10,
-#     'skew_dip': 5
-# }
+
+
