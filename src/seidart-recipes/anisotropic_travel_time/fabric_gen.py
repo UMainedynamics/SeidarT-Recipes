ea5847c81ba8971b84fa62054482f8973e01b459
sbernsen <stevenbernsen@gmail.com>
2024-06-12 14:25:52 -0400

    anisotropic travel time recipe added

---
+++ src/seidart-recipes/anisotropic_travel_time/fabric_gen.py
@@ -0,0 +1,74 @@
+
+import numpy as np 
+import pandas as pd 
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from seidart.routines.fabricsynth import Fabric
+
+# ------------------------------------------------------------------------------
+
+# Create fabrics 
+sp1 = {
+    'distribution': 'normal', 'npts': [200],
+    'trend': [0], 'trend_std': [180], 'skew_trend': [0],
+    'plunge': [0], 'plunge_std': [90], 'skew_plunge': [0]
+}
+sp2 = {
+    'distribution': 'normal', 'npts': [200],
+    'trend': [0],'trend_std': [180],'skew_trend': [0],
+    'plunge': [0],'plunge_std': [65],'skew_plunge': [0]
+}
+sp3 = {
+    'distribution': 'normal','npts': [200],
+    'trend': [0],'trend_std': [180],'skew_trend': [0],
+    'plunge': [0],'plunge_std': [40],'skew_plunge': [0]
+}
+sp4 = {
+    'distribution': 'normal','npts': [200],
+    'trend': [0],'trend_std': [180],'skew_trend': [0],
+    'plunge': [0],'plunge_std': [15],'skew_plunge': [0]
+}
+sp5 = {
+    'distribution': 'normal', 'npts': [200],
+    'trend': [0],'trend_std': [180],'skew_trend': [0],
+    'plunge': [0],'plunge_std': [5],'skew_plunge': [0]
+}
+sp1 = Fabric(sp1, output_filename = 'single_pole1.txt', plot = False)
+sp2 = Fabric(sp2, output_filename = 'single_pole2.txt', plot = False)
+sp3 = Fabric(sp3, output_filename = 'single_pole3.txt', plot = False)
+sp4 = Fabric(sp4, output_filename = 'single_pole4.txt', plot = False)
+sp5 = Fabric(sp5, output_filename = 'single_pole5.txt', plot = False)
+
+# Create a split violin plot to compare the population distributions 
+columns = [
+    'Homogeneous', 'Very Weak', 'Moderately Weak', 'Moderately Strong', 'Strong'
+]
+trends = np.column_stack(
+    [sp1.trends, sp2.trends, sp3.trends, sp4.trends, sp5.trends]
+) * np.pi / 180
+plunges = np.column_stack( 
+    [sp1.plunges, sp2.plunges, sp3.plunges, sp4.plunges, sp5.plunges] 
+) * np.pi / 180
+
+# We want to create a dataframe from the arrays 
+population = np.tile(columns, 2*trends.shape[0])
+category = np.repeat(['trend', 'plunge'], np.prod(trends.shape) )
+data = {
+    'Radians': np.concatenate([trends.reshape(-1,1), plunges.reshape(-1, 1) ]).flatten(),
+    'Population': population.flatten(),
+    'Category': category
+}
+trends_and_plunges = pd.DataFrame(data)
+
+# Plot the distributions for comparison. 
+# Set the theme for the plot
+sns.set_theme(style="dark", rc = {'figure.figsize':(9,5)})
+ax = sns.violinplot(
+    data = trends_and_plunges, 
+    x = "Population", y = "Radians", hue = "Category", 
+    split = True, inner="quart", palette="muted", density_norm = 'width'
+)
+# Update the legend
+ax.legend().set_title('')
+plt.show()
--- src/seidart-recipes/anisotropic_travel_time/travel_times.py
+++ src/seidart-recipes/anisotropic_travel_time/travel_times.py
@@ -3,6 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import copy 
 import seaborn as sns
+import os 
+from glob2 import glob
 
 from seidart.routines import prjrun, sourcefunction
 from seidart.routines.arraybuild import Array 
@@ -11,73 +13,9 @@ from seidart.routines.fabricsynth import Fabric
 
 # ------------------------------------------------------------------------------
 # 
-
-# ------------------------------------------------------------------------------
-
-# Create fabrics 
-sp1 = {
-    'distribution': 'normal', 'npts': [200],
-    'strike': [0], 'strike_std': [180], 'skew_strike': [0],
-    'dip': [0], 'dip_std': [90], 'skew_dip': [0]
-}
-sp2 = {
-    'distribution': 'normal', 'npts': [200],
-    'strike': [0],'strike_std': [180],'skew_strike': [0],
-    'dip': [0],'dip_std': [65],'skew_dip': [0]
-}
-sp3 = {
-    'distribution': 'normal','npts': [200],
-    'strike': [0],'strike_std': [180],'skew_strike': [0],
-    'dip': [0],'dip_std': [40],'skew_dip': [0]
-}
-sp4 = {
-    'distribution': 'normal','npts': [200],
-    'strike': [0],'strike_std': [180],'skew_strike': [0],
-    'dip': [0],'dip_std': [15],'skew_dip': [0]
-}
-sp5 = {
-    'distribution': 'normal', 'npts': [200],
-    'strike': [0],'strike_std': [180],'skew_strike': [0],
-    'dip': [0],'dip_std': [5],'skew_dip': [0]
-}
-sp1 = Fabric(sp1, output_filename = 'single_pole1.txt', plot = False)
-sp2 = Fabric(sp2, output_filename = 'single_pole2.txt', plot = False)
-sp3 = Fabric(sp3, output_filename = 'single_pole3.txt', plot = False)
-sp4 = Fabric(sp4, output_filename = 'single_pole4.txt', plot = False)
-sp5 = Fabric(sp5, output_filename = 'single_pole5.txt', plot = False)
-
-# Create a split violin plot to compare the population distributions 
-columns = [
-    'Homogeneous', 'Very Weak', 'Moderately Weak', 'Moderately Strong', 'Strong'
-]
-strikes = np.column_stack(
-    [sp1.strikes, sp2.strikes, sp3.strikes, sp4.strikes, sp5.strikes]
-) * np.pi / 180
-dips = np.column_stack( 
-    [sp1.dips, sp2.dips, sp3.dips, sp4.dips, sp5.dips] 
-) * np.pi / 180
-
-# We want to create a dataframe from the arrays 
-population = np.tile(columns, 2*strikes.shape[0])
-category = np.repeat(['strike', 'dip'], len(strike) )
-data = {
-    'Radians': np.concatenate([strikes.reshape(-1,1), dips.reshape(-1, 1) ]).flatten(),
-    'Population': population.flatten(),
-    'Category': category
-}
-strikes_and_dips = pd.DataFrame(data)
-
-# Plot the distributions for comparison. 
-# Set the theme for the plot
-sns.set_theme(style="dark", rc = {'figure.figsize':(9,5)})
-ax = sns.violinplot(
-    data = strikes_and_dips, 
-    x = "Population", y = "Radians", hue = "Category", 
-    split = True, inner="quart", palette="muted", scale = 'width'
-)
-# Update the legend
-ax.legend().set_title('')
-plt.show()
+generate_fabric = False 
+if generate_fabric:
+    import fabric_gen
 
 # ------------------------------------------------------------------------------
 # Run a set of seismic simulations for the different fabrics generated above
@@ -87,43 +25,132 @@ prjfile = 'split_material.prj'
 domain, material, seismic,__ = prjrun.domain_initialization(prjfile)
 timevec, fx, fy, fz, srcfn = sourcefunction(seismic, 10000, 'gaus1')
 angfiles = np.array(
-    ['single_pole1.txt','single_pole2.txt','single_pole3.txt','single_pole4.txt','single_pole5.txt']
+    [
+        'single_pole1.txt',
+        'single_pole2.txt',
+        'single_pole3.txt',
+        'single_pole4.txt',
+        'single_pole5.txt'
+    ]
 )
 output_basefiles = np.array(['sp1', 'sp2', 'sp3', 'sp4', 'sp5'])
-material.sort_material_list() 
-material.is_anisotropic[1] = True
+# By default, the material list is 11 character unicode which means that the 
+# filenames of the fabric files will get truncated.
+# When we run prjrun.status_check(...) the function 
+# material.sort_material_list() is also called which will overwrite values using 
+# the material_list variable so we need to change the material_list and not the 
+# individual parameters afterwards.
+material.material_list = material.material_list.astype('U16')
+material.material_list[1,-2] = 'True' 
 
-for ind in range(len(angfiles)):    
-    mat = copy.deepcopy(material)
-    seis = copy.deepcopy(seismic)
-    mat.angfiles = np.array(['', angfiles[ind]])
-    prjrun.status_check(
-        seis, mat, domain, prjfile
-    )
-    # P waves
-    prjrun.runseismic(
-        seis, mat, domain
-    )
-    z = Array('Vz', prjfile, 'receivers.xyz')
-    z.save(output_basefile = output_basefiles[ind] + 'z')
-    # Sx waves 
-    seis.theta = 0
-    prjrun.runseismic(
-        seis, mat, domain
-    )
-    x = Array('Vx', prjfile, 'receivers.xyz')
-    x.save(output_basefile = output_basefiles[ind] + 'x')
-    # Sy waves 
-    seis.phi = 90 
-    prjrun.runseismic(
-        seis, mat, domain
-    )
-    y = Array('Vx', prjfile, 'receivers.xyz')
-    y.save(output_basefile = output_basefiles[ind] + 'y')
+n = len(angfiles)
+theta = np.array([90,0,0])
+phi = np.array([0,90,0])
+channels = np.array(['x','y','z'])
+
+for ii in range(len(theta)):
+    for jj in range(n):
+        print('Seismic modeling of single pole fabric ' + str(jj+1) )   
+        material.material_list[1,-1] = angfiles[jj]
+        seis = copy.deepcopy(seismic)
+        prjrun.status_check(
+            seis, material, domain, prjfile
+        )
+        # P waves
+        print(
+            f'Computing model for source \
+                theta = {theta[ii]}, phi = {phi[ii]}; P waves'
+        )
+        print()
+        seis.theta = theta[jj]
+        seis.phi = phi[jj]
+        prjrun.runseismic(
+            seis, material, domain
+        )
+        for channel in channels:
+            print(f'Creating the array objects for channel {channel}.')
+            print()        
+            arr = Array(f'V{channel}', prjfile, 'receivers.xyz')        
+            arr.seismic.theta = seis.theta 
+            arr.seismic.phi = seis.phi
+            print(f'Saving the array objects for the {channel}-component.')
+            print()
+            arr.save(
+                output_basefile = output_basefiles[jj] + \
+                    f'.theta{theta[ii]}.phi{phi[iI]}.{channel}'
+                )
+    
+        print(
+            f'Deleting model outputs for \
+                theta = {theta[ii]} and phi = {phi[ii]}.'
+        )
+        print()
+        datfiles = glob('V*.dat')
+        for file_path in datfiles:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+    
 
 
+# ==============================================================================
+# Make some plots 
+patterns = np.array([
+    '*.theta90.phi0.*.pkl',
+    '*.theta0.phi0.*.pkl', 
+    '*.theta0.phi90.*.pkl'
+])
+
+# Allocate space. We will load the homogeneous 
+m = int(seismic.time_steps)
+p = len(patterns)
+
+x = np.zeros([m, n, 3])
+y = x.copy() 
+z = x.copy()
+homoxyz = np.zeros([m, 3, p])
 
 
-# Set the anisotropic booleans to True and define 
+for jj in range(p):
+    files = glob(patterns[jj])
+    xi = 0
+    yi = 0
+    zi = 0
+    for file in files:
+        if '.x.' in file:
+            with open(file, 'rb') as f:
+                tempx = pickle.load(f)
+            x[:,xi,jj] = tempx.timeseries[:,1]
+            xi += 1
+        
+        if '.y.' in file:
+            with open(file, 'rb') as f:
+                tempy = pickle.load(f)
+            y[:,yi,jj] = tempy.timeseries[:,1]
+            yi += 1
+        
+        if '.z.' in file:
+            with open(file, 'rb') as f:
+                tempz = pickle.load(f)
+            z[:,zi,jj] = tempz.timeseries[:,1]
+            zi += 1
+    
+    homoxyz[:,0,jj] = tempx.timeseries[:,0]
+    homoxyz[:,1,jj] = tempy.timeseries[:,0]
+    homoxyz[:,2,jj] = tempz.timeseries[:,0]
+    
 
+
+    
+# Sort the files into their
+
+labels = [
+    'Homogeneous', 
+    'Modeled Homogeneous', 
+    'Weak', 
+    'Moderately Weak', 
+    'Moderately Strong', 
+    'Strong'
+]
+# 'Source-x', 'Source-y', 'Source-z'
+# There will be a 3c set of subplots for each of the 3 sources
 # ------------------------------------------------------------------------------
