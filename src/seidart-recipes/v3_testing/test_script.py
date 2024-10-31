0239184105feb58a4a85b03267f6b24f45683641
sbernsen <stevenbernsen@gmail.com>
2024-10-31 04:59:05 -0400

    v3 testing folders added

--- src/seidart-recipes/v3_testing/test_script.py
+++ src/seidart-recipes/v3_testing/test_script.py
@@ -1,5 +1,7 @@
 from seidart.routines.definitions import * 
 from seidart.routines.classes import Domain, Material, Model
+from seidart.visualization.im2anim import build_animation
+from seidart.visualization.slice25d import slicer 
 
 project_file = 'test_model.json'
 
@@ -11,11 +13,38 @@ domain, material, seismic, electromag = loadproject(
     Model()
 )
 
-## Compute the tensor coefficients
-# em.status_check(mat, dom, project_file, append_to_json = True)
+# Compute the tensor coefficients
 material.material_flag = True
-seismic.build(material, domain, recompute_tensors = True)
+seismic.build(material, domain, recompute_tensors = False)
+seismic.kband_check(domain)
 seismic.run()
 
-electromag.build(material, domain, append_to_json = True)
-electromag.run()
+# Create the GIF animation so we can 
+frame_delay = 5 
+frame_interval = 20 
+# alpha_value = 0.3
+
+# build_animation(
+#         project_file, 
+#         'Vz', frame_delay, frame_interval, alpha_value, 
+#         is_single_precision = True
+# )
+
+slicer(project_file, 'Vz', 15, frame_interval, frame_delay)
+
+# ------------------------------------------------------------------------------
+# material.material_flag = True
+# electromag.build(material, domain, recompute_tensors = True)
+# electromag.kband_check(domain)
+# electromag.run()
+
+# frame_delay = 5 
+# frame_interval = 10 
+# alpha_value = 0.3
+
+
+# build_animation(
+#         project_file, 
+#         'Ex', frame_delay, frame_interval, alpha_value, 
+#         is_single_precision = True
+# )
