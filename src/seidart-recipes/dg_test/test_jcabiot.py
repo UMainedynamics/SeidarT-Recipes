56872c8b82bdcf79e658ff41ddcddf1baa705a5e
sbernsen <stevenbernsen@gmail.com>
2026-05-05 15:15:14 -0400

    refactor: replace generic test files with specific test_fullbiot and test_jcabiot validation scripts

--- src/seidart-recipes/dg_test/test_jcabiot.py
+++ src/seidart-recipes/dg_test/test_jcabiot.py
@@ -1,38 +1,22 @@
 import numpy as np
 from seidart.routines.definitions import *
-from seidart.routines.classes import Domain, Material, Model
+from seidart.routines.classes import Domain, Material, Model, JCA
 from seidart.routines.arraybuild import Array
 from seidart.visualization.im2anim import build_animation
 
 # ------------------------------------------------------------------------------
-
 ## Initiate the model and domain objects
 project_file = 'surfacewave_validation.json' 
 receiver_file = 'receivers.xyz'
 dom, mat, seis, em = loadproject(
-    project_file, Domain(), Material(), Model(), Model()
+    project_file, Domain(), Material(), JCA(), Model()
 )
 
-seis.CFL = 1.0/(2.0*np.sqrt(3)) #0.3
 seis.use_multimodal = True
 seis.source_n_octaves = 3
-seis.density_method = 'arithmetic'
-# seis.density_method = 'harmonic'
-# seis.density_method = 'geometric' 
-seis.build(mat, dom, recompute_tensors = False) 
-seis.kband_check(dom)
+seis.build(mat, dom) 
 seis.run()
 
-array_vx = Array('Vx', project_file, receiver_file)
-array_vx.exaggeration = 0.01 
-array_vx.save()
-array_vz = Array('Vz', project_file, receiver_file) 
-array_vz.exaggeration = 0.01
-array_vz.save() 
-if dom.dim == 2.5:
-        array_vy = Array('Vy', project_file, receiver_file) 
-        array_vy.exaggeration = 0.01
-        array_vy.save() 
 
 frame_delay = 5 
 frame_interval = 20 
